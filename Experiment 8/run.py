6475ff73110e8dd7f13621ed65e4f0a760a329ea
Arushi230 <arushigoyal712@gmail.com>
2026-02-26 10:30:56 +0530

    deployment fix

--- Experiment 8/run.py
+++ Experiment 8/run.py
@@ -1,5 +1,10 @@
-from app import app
+import os
+from flask import Flask
 
+app = Flask(__name__)
+
+# routes here...
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
